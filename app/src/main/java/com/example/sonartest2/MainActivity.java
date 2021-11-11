d38697b9f97ed379032b041ea87be2d302545b33
Slava Mytnik <slava.mytnik@gmail.com>
2021-11-11 11:37:18 +0300

    1

---
+++ app/src/main/java/com/example/sonartest2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sonartest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+}
